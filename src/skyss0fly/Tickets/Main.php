7b8c5ef6e11a01dfd9e6844939c6ba62e391d7a6
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2023-11-21 21:26:14 +1000

    Create Main.php

---
+++ src/skyss0fly/Tickets/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace\skyss0fly\Tickets;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase {
+
+
+}
